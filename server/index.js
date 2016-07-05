067c9dc66b00238600200a4a3fe58e6ad981ef48
Weilin Shi <934587911@qq.com>
2016-07-05 15:52:00 +0800

    deploy on linux

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
+#!/usr/bin/env node
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const koa = require('koa');
 const app = new koa();
-const reply = fs.readFileSync('./client/index.html', 'utf8');
+const reply = fs.readFileSync(path.resolve(__dirname, '../client/index.html'), 'utf8');
 
 app.use(ctx => {
 	ctx.body = reply;
 });
 
-app.listen(80, err => {
+app.listen(80, '0.0.0.0', err => {
 	if (err) {
 		throw err;
 	}
 
 	console.log('server is running');
 });
+
+app.on('error', function (err) {
+	console.error(err);
+});
+
