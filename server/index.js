ada30fb2a6c9458e4d9d0bd29a7dc32d63ac8d0d
Weilin <934587911@qq.com>
2016-07-05 14:16:11 +0800

    basic setup for deploy

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 'use strict';
 
+const fs = require('fs');
 const koa = require('koa');
-const app = koa();
+const app = new koa();
+const reply = fs.readFileSync('./client/index.html', 'utf8');
+
+app.use(ctx => {
+	ctx.body = reply;
+});
+
+app.listen(80, err => {
+	if (err) {
+		throw err;
+	}
+
+	console.log('server is running');
+});
